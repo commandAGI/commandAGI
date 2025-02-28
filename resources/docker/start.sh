9f7de52ef0fdefcdf1481df7178026109e6fa0bd
Jacob Valdez <jacobfv123@gmail.com>
2025-02-28 13:21:29 -0600

    save

--- resources/docker/start.sh
+++ resources/docker/start.sh
@@ -10,8 +10,7 @@ sleep 5  # Increased sleep time to ensure display is ready
 touch ~/.Xauthority
 
 # Set up X authentication properly
-xauth generate :0 . trusted
-xauth add $HOST:0 . $(mcookie)
+xauth add :0 . $(mcookie)
 
 # Set display environment variable
 export DISPLAY=:0
@@ -26,4 +25,4 @@ sleep 2  # Give VNC server time to initialize
 
 # Activate virtual environment and run daemon in foreground
 source .venv/bin/activate
-exec python3 -m commandLAB.daemon.cli --port 8000 --backend pynput
+python3 -m commandLAB.daemon.cli --port 8000 --backend pynput
